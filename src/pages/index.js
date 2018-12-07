add2465bdae09e7eac8cb00cf3540acf16b640bb
Patrick Murray <contact@patmurraydev.com>
2018-12-07 16:25:12 +1100

    Add new Open Graph tags
    including Twitter and go:image

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,14 +10,17 @@ const IndexPage = () => (
   <React.Fragment>
     <Helmet>
       <title>An Open Letter to Bill Shorten from the Tech Community</title>
-      <meta
-        property="og:title"
-        content="ALP FAIL: An Open Letter to Bill Shorten from the Tech Community"
-      />
-      <meta
-        property="og:description"
-        content="We are angry about #aabill - a bad law that will destroy the IT industry"
-      />
+      <meta name="twitter:card" content="summary_large_image">
+      <meta name="twitter:title" content="ALP FAIL: An Open Letter to Bill Shorten from the Tech Community">
+      <meta name="twitter:description" content="We are angry about #aabill - a bad law that will destroy the IT industry">
+      <meta name="twitter:image" content={ogimage}>
+
+      <meta property="og:title" content="ALP FAIL: An Open Letter to Bill Shorten from the Tech Community" />
+      <meta property="og:type" content="website" />
+      <meta property="og:image" content={ogimage} />
+      <meta property="og:image:secure_url" content={ogimage} />
+      <meta property="og:description" content="We are angry about #aabill - a bad law that will destroy the IT industry" /> 
+  
     </Helmet>
     <ThemeProvider>
       <Header />
