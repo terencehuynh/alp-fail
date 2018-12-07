3b1f6213624b1b3993e5c381683180ebffcabf8b
Patrick Murray <contact@patmurraydev.com>
2018-12-07 16:28:59 +1100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,10 +10,10 @@ const IndexPage = () => (
   <React.Fragment>
     <Helmet>
       <title>An Open Letter to Bill Shorten from the Tech Community</title>
-      <meta name="twitter:card" content="summary_large_image">
-      <meta name="twitter:title" content="ALP FAIL: An Open Letter to Bill Shorten from the Tech Community">
-      <meta name="twitter:description" content="We are angry about #aabill - a bad law that will destroy the IT industry">
-      <meta name="twitter:image" content={ogimage}>
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="ALP FAIL: An Open Letter to Bill Shorten from the Tech Community" />
+      <meta name="twitter:description" content="We are angry about #aabill - a bad law that will destroy the IT industry" />
+      <meta name="twitter:image" content={ogimage} />
 
       <meta property="og:title" content="ALP FAIL: An Open Letter to Bill Shorten from the Tech Community" />
       <meta property="og:type" content="website" />
