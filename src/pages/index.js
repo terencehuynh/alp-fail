9b45c49317e60eda08b8a4cc0fb17dbc3d5b5d18
Patrick Murray <contact@patmurraydev.com>
2018-12-07 16:30:23 +1100

    Forgot to import the image

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,8 @@ import { Header } from '../components/header'
 import { Footer } from '../components/footer'
 import { Letter } from '../components/letter'
 
+import ogimage from '../components/ogimage.jpg'
+
 const IndexPage = () => (
   <React.Fragment>
     <Helmet>
