0f13852b5714539d76b4e2394eee684063e89afc
Terence Huynh <me@terencehuynh.com>
2018-12-08 20:36:31 +1100

    Meta tags

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,16 +13,30 @@ const IndexPage = () => (
     <Helmet>
       <title>An Open Letter to Bill Shorten from the Tech Community</title>
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content="ALP FAIL: An Open Letter to Bill Shorten from the Tech Community" />
-      <meta name="twitter:description" content="We are angry about #aabill - a bad law that will destroy the IT industry" />
-      <meta name="twitter:image" content={"https://alp.fail" + ogimage} />
+      <meta
+        name="twitter:title"
+        content="ALP FAIL: An Open Letter to Bill Shorten from the Tech Community"
+      />
+      <meta
+        name="twitter:description"
+        content="We are angry about #aabill - a bad law that will destroy the IT industry"
+      />
+      <meta name="twitter:image" content={'https://alp.fail' + ogimage} />
 
-      <meta property="og:title" content="ALP FAIL: An Open Letter to Bill Shorten from the Tech Community" />
+      <meta
+        property="og:title"
+        content="ALP FAIL: An Open Letter to Bill Shorten from the Tech Community"
+      />
       <meta property="og:type" content="website" />
-      <meta property="og:image" content={"https://alp.fail" + ogimage} />
-      <meta property="og:image:secure_url" content={"https://alp.fail" + ogimage} />
-      <meta property="og:description" content="We are angry about #aabill - a bad law that will destroy the IT industry" /> 
-  
+      <meta property="og:image" content={'https://alp.fail' + ogimage} />
+      <meta
+        property="og:image:secure_url"
+        content={'https://alp.fail' + ogimage}
+      />
+      <meta
+        property="og:description"
+        content="We are angry about #aabill - a bad law that will destroy the IT industry"
+      />
     </Helmet>
     <ThemeProvider>
       <Header />
